94e112bd6aed8a3047fc3bc3d9bd6eb3ee307ffe
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-09-17 10:25:10 -0600

    Update How Many Numbers Are Smaller Than the Current Number.js

--- How Many Numbers Are Smaller Than the Current Number.js
+++ How Many Numbers Are Smaller Than the Current Number.js
@@ -1,4 +1,4 @@
-/*Challenge 1: Solve the problem for given input [8,1,2,2,3] - In this challenge, implement the smallerNumbersThanCurrent function so that it returns the correct output when given the input array [8,1,2,2,3].
+/*Challenge 1: Solve the problem for given input [8,1,2,2,3] - In this challenge, implement the smallerNumbersThanCurrent function so that it returns the correct output when given the input array [8,1,2,2,3]. 
 
 Challenge 2: Solve the problem for given input [6,5,4,8] - Implement the function for another input [6,5,4,8]. Make sure your function works for different inputs and provides expected outputs.*/
 
